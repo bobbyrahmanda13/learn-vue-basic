ddc2475a1fcd4e760ee884b38e94c47094062d66
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2022-08-02 15:41:00 +0700

    learn dynamic Component part1-2

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="modals">
     <h1>Modals</h1>
+    <div>
+      <label for="">Show dark Modals</label>
+      <input v-model="showDarkModals" type="checkbox" />
+    </div>
+    <!-- <pre>{{ showDarkModals }}</pre> -->
     <button @click="showModal = true">Show Modal</button>
-    <Modal v-model="showModal" title="My new Title (via props)">
-      <!-- <template v-slot:title>My new Title </template> -->
-      <!-- <template #title>My new Title </template>  learn slots-->
+    <!-- <Modal v-model="showModal" title="My new Title (via props)"> -->
+    <!-- <template v-slot:title>My new Title </template>
+      <template #title>My new Title </template>  learn slots -->
+    <!-- <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+        optio, dolores laudantium quidem in aliquid itaque earum nostrum, ipsa
+        beatae quis tenetur natus quos corporis, hic commodi dolor veniam non!
+      </p>
+    </Modal> -->
+
+    <component :is="showDarkModals ? ModalDark : Modal" v-model="showModal" title="My new Title (via props)">
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
         optio, dolores laudantium quidem in aliquid itaque earum nostrum, ipsa
         beatae quis tenetur natus quos corporis, hic commodi dolor veniam non!
       </p>
-    </Modal>
+    </component>
   </div>
 </template>
 
@@ -18,7 +31,10 @@
 // import
 import { ref } from "vue";
 import Modal from "@/components/Modal.vue";
+import ModalDark from "@/components/ModalDark.vue";
+
 // modals
+const showDarkModals = ref(false);
 const showModal = ref(false);
 </script>
 <!-- option api -->
