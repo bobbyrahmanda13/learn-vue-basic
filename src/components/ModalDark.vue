9737e112ba76ca9067582f7702fca2b90833a682
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2022-08-02 15:41:36 +0700

    add modaldark

---
+++ src/components/ModalDark.vue
@@ -0,0 +1,36 @@
+<template>
+  <Teleport to=".modals-containers">
+    <div class="modal-dark" v-if="modelValue">
+      <h1>{{ title }}</h1>
+      <slot></slot>
+      <button @click="$emit('update:modelValue', false)">Hide Modal</button>
+    </div>
+  </Teleport>
+</template>
+<script setup>
+// props
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: "No title specified",
+  },
+});
+</script>
+
+<style scoped>
+.modal-dark {
+  background: #333;
+  color: white;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
